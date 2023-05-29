a18936e2e325a518bb3d2b3f4e3403b3c4fc0af4
Yuri Lima <yuriibrasil@gmail.com>
2023-05-29 10:33:52 -0300

    Mandando pro PC

--- src/App.js
+++ src/App.js
@@ -1,60 +1,25 @@
-import './Atividades.css';
-// import * as Site from './components/05Multiplos'
-// import { Supermercado, Legume, Fruta, Bebida } from './components/06Children';
-// import {World, Arena} from './components/atividades/atividade01/03Batalha'
-import FunctionContextA from './components/atividades/atividade02/questao01/FunctionContextA';
-import ComponenteAvo from './components/atividades/atividade02/questao02/ComponenteAvo';
-import Cidades from './components/atividades/atividade03/questao01/Cidades';
-import Pai from './components/atividades/atividade03/questao02/Pai';
-import PokemonAxios from './components/11Axios/PokemonAxios';
-import { Questao01A } from './components/provaPIW/Questao01';
-import Questao02 from './components/provaPIW/Questao02';
-import Questao03 from './components/provaPIW/Questao03';
-// import TelaPrincipal from './components/12Redux/v2/TelaPrincipal';
-// import MeuIndex from './components/12Redux/v2/MeuIndex';
-// import MinhaPromise from './components/13Promisses/MinhaPromessa';
-
-// function App() {
-//   return (
-//     <>
-//       <Site.Header />
-//       <Site.Body />
-//     </>
-//   );
-// }
-
-// export default App;
-
-// function App() {
-//   return (
-//     <div className="estilo-div">
-//       <Temperatura/>
-//     </div> 
-//   )
-// }
-
-// export default App;
-
-// function App() {
-//   return (
-//     <>
-//       <Supermercado nome="badu">
-//         <Bebida nome="Coca"></Bebida>
-//         <Legume nome="Chero"></Legume>
-//         <Fruta nome="Maça"></Fruta>
-//       </Supermercado>
-//     </>
-//   );
-// }
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    // <Questao01A>
-
-    // </Questao01A>
-    // <Questao02></Questao02>
-    <Questao03></Questao03>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
